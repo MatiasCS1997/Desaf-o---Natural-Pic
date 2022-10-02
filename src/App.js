2536d91e066799ee1880b141af93b582aa4584c1
MatiasCS1997 <mcorderos@miucsh.cl>
2022-10-02 20:05:15 -0300

    app js edit - components- context

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import NavbarSearch from "./components/NavbarSearch";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ContextProvider } from "./context/provider";
+import Home from "./components/Home";
+import FavoritesPhotos from "./components/FavoritesPhotos";
+// import Contact from "./components/Contact";
+
+function App() {
+  return (
+    <ContextProvider>
+      <BrowserRouter>
+        <NavbarSearch />;
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/favoritesPhotos" element={<FavoritesPhotos />} />
+        </Routes>
+      </BrowserRouter>
+    </ContextProvider>
+  );
+}
+
+export default App;
